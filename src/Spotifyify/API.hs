887c32d43bab3377476036a09e4a2333a5b3dbb6
Chris Birchall <chris.birchall@gmail.com>
2019-04-11 23:25:00 +0100

    Implement following of artists
    Also rate limiting

--- src/Spotifyify/API.hs
+++ src/Spotifyify/API.hs
@@ -3,10 +3,14 @@
 
 module Spotifyify.API where
 
-import Data.ByteString.Char8 (pack)
+import Prelude hiding (id)
+import Data.ByteString.Char8 (pack, readInt)
+import Data.Maybe (fromMaybe, listToMaybe)
 import Data.Text (Text, intercalate)
 import Data.Text.Encoding (encodeUtf8)
+import Control.Concurrent (threadDelay)
 import Network.HTTP.Simple
+import Network.HTTP.Types.Status
 import Network.OAuth.OAuth2 (OAuth2Token, accessToken, atoken)
 import Data.Aeson (FromJSON, ToJSON, toJSON, decodeStrict)
 import GHC.Generics (Generic)
@@ -45,15 +49,49 @@ searchArtists query offset = sendRequest request
       ]
     request = addQueryParams <$> request'
 
--- TODO check for 429 response -> sleep and retry
+followArtist :: Artist -> OAuth2Token -> IO ()
+followArtist artist = fireAndForget request
+  where
+    request' = parseRequest "PUT https://api.spotify.com/v1/me/following"
+    addQueryParams =
+      setRequestQueryString [
+        ("ids", Just $ encodeUtf8 (id artist)),
+        ("type", Just "artist")
+      ]
+    request = addQueryParams <$> request'
+
+fireAndForget :: IO Request -> OAuth2Token -> IO ()
+fireAndForget mkRequest oauth = do
+  request' <- mkRequest
+  let request = withAuth oauth request'
+  response <- withRateLimiting $ httpNoBody request
+  return ()
+
 sendRequest :: FromJSON a => IO Request -> OAuth2Token -> IO a
 sendRequest mkRequest oauth = do
   request' <- mkRequest
   let request = withAuth oauth request'
-  response <- httpJSON request
+  response <- withRateLimiting $ httpJSON request
   pure $ getResponseBody response
 
 withAuth :: OAuth2Token -> Request -> Request
 withAuth oauth req = addRequestHeader "Authorization" bearer req
   where
     bearer = encodeUtf8 $ intercalate " " [ "Bearer ", (atoken . accessToken) oauth ]
+
+withRateLimiting :: IO (Response a) -> IO (Response a)
+withRateLimiting sendRequest = do
+  response <- sendRequest
+  case getResponseStatus response of
+    s | s == status429 -> do
+      putStrLn $ "Hit the rate limiting. Sleeping for " ++ (show $ retryAfter response) ++ " seconds"
+      threadDelay $ (retryAfter response) * 1000000
+      withRateLimiting sendRequest
+    _ -> return response
+
+retryAfter :: Response a -> Int
+retryAfter response = fromMaybe 5 headerValue
+  where
+    header = listToMaybe $ getResponseHeader "Retry-After" response
+    headerValue = fst <$> (header >>= readInt)
+
--- src/Spotifyify/Import.hs
+++ src/Spotifyify/Import.hs
@@ -2,31 +2,33 @@ module Spotifyify.Import where
 
 import Data.List (find)
 import Data.Text (Text, unpack)
-import Control.Concurrent (threadDelay)
 import System.IO (Handle)
 import Network.OAuth.OAuth2 (OAuth2Token)
 import Spotifyify.Manifest (Manifest(..), Artist(..), name)
 import Spotifyify.Log (LogEntry(..), writeEntry)
 import Spotifyify.Auth (initOAuth2Data, authenticate)
-import Spotifyify.API (ArtistSearchResult(..), Artists(..), searchArtists)
-import qualified Spotifyify.API as API (Artist, name)
+import qualified Spotifyify.API as API (ArtistSearchResult(..), Artists(..), Artist, name, searchArtists, followArtist)
 
 performImport :: Manifest -> Handle -> IO ()
 performImport (Manifest artists) logHandle = do
   oauth2Data <- initOAuth2Data
   oauth2Token <- authenticate oauth2Data
-  putStrLn $ show oauth2Token
   sequence $ (importArtist oauth2Token logHandle) <$> artists
   return ()
 
 importArtist :: OAuth2Token -> Handle -> Artist -> IO ()
 importArtist oauth logHandle artist@(Artist name albums) = do
-  artist <- findArtist artist oauth
-  case artist of
-    Just a -> putStrLn $ "Found artist: " ++ (unpack $ API.name a)
-    Nothing -> putStrLn $ "Could not find any artist to match " ++ show artist
-  writeEntry logHandle (LogEntry name True (length albums))
-  threadDelay 5000000
+  result <- findArtist artist oauth
+  case result of
+    Just a -> do
+      putStrLn $ "Found artist: " ++ (unpack $ API.name a)
+      API.followArtist a oauth
+      putStrLn $ "Followed " ++ (unpack $ API.name a)
+      -- TODO save albums
+      writeEntry logHandle (LogEntry name True (length albums))
+    Nothing -> do
+      putStrLn $ "Could not find artist in Spotify: " ++ unpack name
+      writeEntry logHandle (LogEntry name False 0)
 
 findArtist :: Artist -> OAuth2Token -> IO (Maybe API.Artist)
 findArtist artist oauth = searchFromOffset 0
@@ -34,22 +36,22 @@ findArtist artist oauth = searchFromOffset 0
     searchFromOffset :: Int -> IO (Maybe API.Artist)
     searchFromOffset offset = searchAtOffset offset >>= handlePage
 
-    searchAtOffset :: Int -> IO ArtistSearchResult
-    searchAtOffset offset = searchArtists (name artist) offset oauth
+    searchAtOffset :: Int -> IO API.ArtistSearchResult
+    searchAtOffset offset = API.searchArtists (name artist) offset oauth
 
-    findInPage :: ArtistSearchResult -> Maybe API.Artist
-    findInPage (ArtistSearchResult (Artists [x] _ _ 1)) =
+    findInPage :: API.ArtistSearchResult -> Maybe API.Artist
+    findInPage (API.ArtistSearchResult (API.Artists [x] _ _ 1)) =
       Just x
-    findInPage (ArtistSearchResult (Artists items _ _ total)) =
+    findInPage (API.ArtistSearchResult (API.Artists items _ _ total)) =
       find (matchesArtistName artist) items
 
-    handlePage :: ArtistSearchResult -> IO (Maybe API.Artist)
+    handlePage :: API.ArtistSearchResult -> IO (Maybe API.Artist)
     handlePage page = case findInPage page of
                         Just artist -> return (Just artist)
                         Nothing -> giveUpOrContinue page
 
-    giveUpOrContinue :: ArtistSearchResult -> IO (Maybe API.Artist)
-    giveUpOrContinue (ArtistSearchResult (Artists _ _ offset total)) =
+    giveUpOrContinue :: API.ArtistSearchResult -> IO (Maybe API.Artist)
+    giveUpOrContinue (API.ArtistSearchResult (API.Artists _ _ offset total)) =
       if offset + pageSize >= total
          then return Nothing -- give up
          else searchFromOffset (offset + pageSize) -- recurse
